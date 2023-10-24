d8dba3b1a31ca5281fe1ec3262a24406a6632345
fkhideaki <fkhideaki@gmail.com>
2023-10-24 13:32:33 +0900

    Update Process.py

--- samples/std/Process.py
+++ samples/std/Process.py
@@ -16,5 +16,20 @@ def printProcList():
 			continue
 		print(str(proc.pid) + ' : ' + path)
 
+def findProcByPath(filePath):
+	for proc in psutil.process_iter():
+		if proc.pid == 0:
+			continue
+		path = getExePath(proc)
+		if path == filePath:
+			return proc
+	return None
+
 def openDirExplorer(dir):
 	subprocess.Popen(['explorer', dir], shell=True)
+
+def restartProc(filePath):
+	p = findProcByPath(filePath)
+	if not p is None:
+		p.kill()
+	subprocess.Popen(filePath)
