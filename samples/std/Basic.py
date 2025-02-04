efc865795d7d92e7a028374f5baf754a40424cbc
fkhideaki <fkhideaki@gmail.com>
2025-02-04 14:43:10 +0900

    Update Basic.py

--- samples/std/Basic.py
+++ samples/std/Basic.py
@@ -1,2 +1,14 @@
 def indexToStr(idx, len):
 	return ('0' * len + str(idx))[-len:]
+
+class CurrentDir:
+    def __init__(self, wsDir):
+        self.iniDir = os.getcwd()
+        self.wsDir = wsDir
+
+    def __enter__(self):
+        os.chdir(self.wsDir)
+        return self
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        os.chdir(self.iniDir)
