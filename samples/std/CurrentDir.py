4cab19536aa91cf1793f397afd600f1bd60f66b5
fkhideaki <fkhideaki@gmail.com>
2025-04-07 08:58:13 +0900

    Create CurrentDir.py

---
+++ samples/std/CurrentDir.py
@@ -0,0 +1,13 @@
+import os
+
+class CurrentDir:
+	def __init__(self, wsDir):
+		self.iniDir = os.getcwd()
+		self.wsDir = wsDir
+
+	def __enter__(self):
+		os.chdir(self.wsDir)
+		return self
+
+	def __exit__(self, exception_type, exception_value, traceback):
+		os.chdir(self.iniDir)
