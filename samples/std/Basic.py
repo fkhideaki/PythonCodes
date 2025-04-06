aa04c41ddd5e1539d6f7acc42b16fd66c3116327
fkhideaki <fkhideaki@gmail.com>
2025-04-07 08:54:15 +0900

    Update Basic.py

--- samples/std/Basic.py
+++ samples/std/Basic.py
@@ -3,6 +3,15 @@ import os
 def indexToStr(idx, len):
 	return ('0' * len + str(idx))[-len:]
 
+def dirp(obj):
+    """非公開をメンバ一覧"""
+    a = []
+    for s in dir(obj):
+        if s.startswith('__'): continue
+        if s.endswith('__'): continue
+        a.append(s)
+    return a
+
 class CurrentDir:
 	def __init__(self, wsDir):
 		self.iniDir = os.getcwd()
