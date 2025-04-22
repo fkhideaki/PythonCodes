d4f9f1248c3add0d1ee2a9a6c22ecfc8d621bed8
fkhideaki <fkhideaki@gmail.com>
2025-04-23 08:46:33 +0900

    update

--- samples/std/GZip.py
+++ samples/std/GZip.py
@@ -2,6 +2,5 @@ import gzip
 import shutil
 
 def makeGZip(src, dst):
-    with open(src, mode='rb') as f:
-        with gzip.open(dst, mode='wb') as z:
-            shutil.copyfileobj(f, z)
+    with open(src, mode='rb') as f, gzip.open(dst, mode='wb') as z:
+        shutil.copyfileobj(f, z)
