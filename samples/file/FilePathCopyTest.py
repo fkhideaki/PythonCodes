5c7d5042a1df38754cb1034f51090866e373e7df
fkhideaki <fkhideaki@gmail.com>
2026-03-22 16:50:32 +0900

    ファイルパスコピー処理のテストコード

---
+++ samples/file/FilePathCopyTest.py
@@ -0,0 +1,44 @@
+'''
+クリップボードからファイルパスを取得するサンプル。
+エクスプローラでファイルを直接コピーしている場合はそのパス
+テキストでパスをコピーしている場合はそれらをリストにしたものを返す
+'''
+
+import pyperclip
+# pip install pywin32
+import win32clipboard
+import win32con
+
+def getClipboardFilesExplorer():
+    win32clipboard.OpenClipboard()
+    try:
+        if win32clipboard.IsClipboardFormatAvailable(win32con.CF_HDROP):
+            data = win32clipboard.GetClipboardData(win32con.CF_HDROP)
+            return list(data)
+        else:
+            return []
+    finally:
+        win32clipboard.CloseClipboard()
+
+def getClipboardFiles():
+    files = getClipboardFilesExplorer()
+    if files:
+        return files
+    c = pyperclip.paste()
+    files = c.splitlines()
+    if files:
+        v = []
+        for s in files:
+            if s[0] == '"' and s[-1] == '"':
+                s = s[1:-1]
+            v.append(s)
+        return v
+    return None
+
+def main():
+    files = getClipboardFiles()
+    for f in files:
+        print(f)
+
+if __name__ == "__main__":
+    main()
